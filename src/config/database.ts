5cfc4d0489d623b3ae1cd4b0596fe0228829a75f
Dinesh Budhathoki <physmarika@gmail.com>
2025-06-29 12:10:37 +0800

    Database connection is successfull using typescript

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,2 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import mongoose from "mongoose";
-const connectD;
+const connectDB = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("Database connection successful");
+  } catch (error) {
+    console.error("Database connection failed:");
+    process.exit(1); // Exit the process if the database connection fails
+  }
+};
+
+export default connectDB;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import connectDB from "./config/database";
 const app = express();
 const port = 8000;
 //connnect MongoDb
 connectDB();
 
 app.use(express.json());
-app.use("/api/phone-directory", phoneDirectoryRoutes);
+// app.use("/api/phone-directory", phoneDirectoryRoutes);
 
 app.get("/", (req, res) => {
   res.send("welcome to the phone directory");
